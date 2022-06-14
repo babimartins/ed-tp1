3dfb4683a91d633ad1dfda51fefcc38f8e35eabb
Bárbara Martins <martins.barbara04@gmail.com>
2022-06-13 23:01:43 -0300

    Add invalid round test

--- TP1/src/round.cpp
+++ TP1/src/round.cpp
@@ -17,58 +17,79 @@ Round::Round(int numPlays, int initBet, VectorCustom<Play> plays, Result result)
 }
 
 void Round::generateResult(VectorCustom<Player> players) {
+    bool isInvalidRound = false;
     for (int i = 0; i < players.length(); ++i) {
-        Play play;
         Player player = players.get(i);
+        int totalAmount = player.getTotalAmount();;
+        Play play;
         for (int j = 0; j < _plays.length(); ++j) {
             if (_plays.get(j).getPlayerName() == player.getPlayerName()) {
                 play = _plays.get(j);
                 break;
             }
         }
-        int winnerAmount = player.getTotalAmount();
         int bet = _initBet + play.getBet();
-        player.setTotalAmount(winnerAmount - bet);
-        players.push(player, i);
-    }
-    VectorCustom<Play> updatedPlays;
-    for (int i = 0; i < _plays.length(); ++i) {
-        Play play = _plays.get(i);
-        play.generatePlayClassification();
-        play.generatePlayValue();
-        updatedPlays.push(play);
-    }
-    updatedPlays.sort();
-    setPlays(updatedPlays);
-    Play lastWinner = _plays.get(_plays.length() - 1);
-    VectorCustom<Play> tieWinners;
-    for (int i = 0; i < _plays.length(); ++i) {
-        Play play = _plays.get(i);
-        if (play.getValue() == lastWinner.getValue()) {
-            tieWinners.push(play);
+        if (totalAmount < bet) {
+            isInvalidRound = true;
         }
     }
-    VectorCustom<Play> winners = Helper::getWinnersTieBreak(tieWinners);
-    VectorCustom<std::string> winnersNames;
-    for (int i = 0; i < winners.length(); ++i) {
-        Play play = winners.get(i);
-        winnersNames.push(play.getPlayerName());
-    }
-    int totalAmount = (_initBet * players.length());
-    for (int i = 0; i < _plays.length(); ++i) {
-        totalAmount += _plays.get(i).getBet();
-    }
-    totalAmount /= winners.length();
-    for (int i = 0; i < players.length(); ++i) {
-        if (winnersNames.contains(players.get(i).getPlayerName())) {
-            Player winner = players.get(i);
-            int winnerAmount = winner.getTotalAmount();
-            winner.setTotalAmount(winnerAmount + totalAmount);
-            players.push(winner, i);
+    if (isInvalidRound) {
+        Result result = Result(0, 0, "I", VectorCustom<std::string>());
+        setResult(result);
+    } else {
+        for (int i = 0; i < players.length(); ++i) {
+            Player player = players.get(i);
+            Play play;
+            for (int j = 0; j < _plays.length(); ++j) {
+                if (_plays.get(j).getPlayerName() == player.getPlayerName()) {
+                    play = _plays.get(j);
+                    break;
+                }
+            }
+            int winnerAmount = player.getTotalAmount();
+            int bet = _initBet + play.getBet();
+            player.setTotalAmount(winnerAmount - bet);
+            players.push(player, i);
         }
+        VectorCustom<Play> updatedPlays;
+        for (int i = 0; i < _plays.length(); ++i) {
+            Play play = _plays.get(i);
+            play.generatePlayClassification();
+            play.generatePlayValue();
+            updatedPlays.push(play);
+        }
+        updatedPlays.sort();
+        setPlays(updatedPlays);
+        Play lastWinner = _plays.get(_plays.length() - 1);
+        VectorCustom<Play> tieWinners;
+        for (int i = 0; i < _plays.length(); ++i) {
+            Play play = _plays.get(i);
+            if (play.getValue() == lastWinner.getValue()) {
+                tieWinners.push(play);
+            }
+        }
+        VectorCustom<Play> winners = Helper::getWinnersTieBreak(tieWinners);
+        VectorCustom<std::string> winnersNames;
+        for (int i = 0; i < winners.length(); ++i) {
+            Play play = winners.get(i);
+            winnersNames.push(play.getPlayerName());
+        }
+        int totalAmount = (_initBet * players.length());
+        for (int i = 0; i < _plays.length(); ++i) {
+            totalAmount += _plays.get(i).getBet();
+        }
+        totalAmount /= winners.length();
+        for (int i = 0; i < players.length(); ++i) {
+            if (winnersNames.contains(players.get(i).getPlayerName())) {
+                Player winner = players.get(i);
+                int winnerAmount = winner.getTotalAmount();
+                winner.setTotalAmount(winnerAmount + totalAmount);
+                players.push(winner, i);
+            }
+        }
+        Result result = Result(winners.length(), totalAmount, lastWinner.getClassification(), winnersNames);
+        setResult(result);
     }
-    Result result = Result(winners.length(), totalAmount, lastWinner.getClassification(), winnersNames);
-    setResult(result);
 }
 
 void Round::print() {
